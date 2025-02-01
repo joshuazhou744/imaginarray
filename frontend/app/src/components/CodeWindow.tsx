e307a6e96795530449891abed235ee75c8eee41c
Jacob <Jacob.leader06@gmail.com>
2025-02-01 14:51:48 -0500

    page litewally looks so freeking gucci uwu xoxo

--- frontend/app/src/components/CodeWindow.tsx
+++ frontend/app/src/components/CodeWindow.tsx
@@ -2,14 +2,17 @@ import { UnControlled as CodeMirror } from "react-codemirror2";
 import "codemirror/lib/codemirror.css";
 import "codemirror/mode/python/python";
 import "../styles/CodeWindow.css";
+import "codemirror/theme/ayu-dark.css";
 
 
 export default function CodeWindow() {
     const options = {
         lineNumbers: true,
+        lineWrapping: true,
         mode: "python",
+        theme: "ayu-dark",
     }
-
+    
     return (
         <div className="CodeWindow">
             <CodeMirror
