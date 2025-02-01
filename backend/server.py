c3be16385d23a218cedaf439ccc0458d8b415b59
Jacob <Jacob.leader06@gmail.com>
2025-02-01 16:12:57 -0500

    marsive server improvements (lit)

--- backend/server.py
+++ backend/server.py
@@ -1,20 +1,41 @@
-from flask import Flask
-import json
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+#! CHANGE ORIGIN ONCE IN PROD
+CORS(app, origins="http://localhost:5173", supports_credentials=True)
 
-@app.route('/submit-code')
-def process_code(code):
-    '''
-    input: [String] <- each item is a line of code
-    return: [] <- all the animations needed
-    '''
-    print(code)
-    return 
+@app.route("/submit_code", methods=["POST"])
+def submit_code():
+    print(request.json)
+    code = request.json.get("code")
+    code = clean_code(code)
+    
+    # for line in code:
+    #     print(line)
 
-# def clean_string(line):
     
+    initial_arr = None
+    manipulations = None
+    final_arr = None
     
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=4000)
+    return jsonify({
+        "message": "manipulations created", 
+        "initial_arr": initial_arr,
+        "manipulations": manipulations,
+        "final_arr": final_arr,
+    })
+    
+def clean_code(code):
+    '''
+    adds tab chars
+    removes empty lines
+    '''
+    cleaned_code = [line for line in code if line.strip()] # Remove empty lines
+    
+    #? Maybe do smth with tabs later (use \t)
+    return cleaned_code
+        
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=4000, debug=True)
--- frontend/app/src/components/CodeWindow.tsx
+++ frontend/app/src/components/CodeWindow.tsx
@@ -3,25 +3,41 @@ import "codemirror/lib/codemirror.css";
 import "codemirror/mode/python/python";
 import "../styles/CodeWindow.css";
 import "codemirror/theme/oceanic-next.css";
+import axios from 'axios';
+import { useState } from "react";
+
 
 
 export default function CodeWindow() {
+    const [code, setCode] = useState("");
+
     const options = {
         lineNumbers: true,
         lineWrapping: true,
         mode: "python",
         theme: "oceanic-next"
+        
     }
     
     return (
         <div className="CodeWindow">
             <CodeMirror
               options={options}
+              value={code}
+              onChange={(_editor, _data, value) => setCode(value)}
             />
-            <button className="vButton">
+            <button className="vButton" onClick={() => parseCode(code)}>
                 Visualize
             </button>
 
         </div>
     );
+}
+
+const parseCode = async(code: string) => {
+    const lines = code.split("\n");
+    console.log(lines)
+    const response = await axios.post('http://172.30.145.175:4000/submit_code', {
+       code: lines 
+    })
 }
