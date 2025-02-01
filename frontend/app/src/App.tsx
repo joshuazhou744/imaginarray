5d4f795e794ac36e96019689c651e2367c0637e3
Jacob <Jacob.leader06@gmail.com>
2025-02-01 16:56:28 -0500

    Post from App instead of CodeWindow

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,32 +1,42 @@
-// App.tsx
-import { FC } from 'react';
-import ArrayVisualizer from "./components/ArrayVisualizer"
+import { FC, useState } from "react";
+import ArrayVisualizer from "./components/ArrayVisualizer";
 import { Manipulation } from "./utils/manipulateTypes";
 import CodeWindow from './components/CodeWindow';
+import axios from 'axios';
 import './styles/App.css';
 
 const App: FC = () => {
+  const [initialArray, setInitialArray] = useState<number[]>([]);
+  const [manipulations, setManipulations] = useState<Manipulation<unknown>[]>([]);
 
-  const initialArray = [1, 2, 3];
+  const parseCode = async (code: string) => {
+    try {
+      const lines = code.split("\n");
+      const response = await axios.post('http://172.30.145.175:4000/submit_code', {
+        code: lines,
+      });
 
-  const manipulations: Manipulation<unknown>[] = [
-    { type: 'append', value: 2 },
-    { type: 'append', value: 3 },
-    { type: 'swap', indices: [0, 2] },
-    {type: "reverse"}
-  ];
+      setInitialArray(response.data.initial_arr || []); 
+      setManipulations(response.data.manipulations || []);  
+      console.log(response.data)
+      console.log(initialArray)
+      console.log(manipulations)
+    } catch (error) {
+      console.error('DAMMIT ERROR :( :\n', error);
+    }
+  };
 
   return (
     <div className="split-screen-container">
-      <div className='left'>
-          <CodeWindow/>
+      <div className="left">
+        <CodeWindow parseCode={parseCode} />
       </div>
-      <div className='right'>
+      <div className="right">
         <h2>Array Manipulation Visualization</h2>
         <ArrayVisualizer initialArray={initialArray} manipulations={manipulations}/>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
